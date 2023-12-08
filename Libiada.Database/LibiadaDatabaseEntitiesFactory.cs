9509a8ed2bab527a2d0f1b3724843a5d3eedcb65
Nikolay Pozdnichenko <nick680@gmail.com>
2023-12-08 20:26:45 +0600

    Update nuget Npgsql
    Also added npgsql configuration class.

--- Libiada.Database/LibiadaDatabaseEntitiesFactory.cs
+++ Libiada.Database/LibiadaDatabaseEntitiesFactory.cs
@@ -11,6 +11,6 @@
             this.configuration = configuration;
         }
 
-        public LibiadaDatabaseEntities Create() => new LibiadaDatabaseEntities("metadata=res://*/LibiadaWeb.csdl|res://*/LibiadaWeb.ssdl|res://*/LibiadaWeb.msl;provider=Npgsql;provider connection string=&quot;Server=localhost;Port=5434;Database=test;UserId=postgres;CommandTimeout=10000000;enlist=true;&quot;");
+        public LibiadaDatabaseEntities Create() => new LibiadaDatabaseEntities(configuration.GetConnectionString("LibiadaDatabaseEntities"));
     }
 }
---
+++ Libiada.Database/NpgSqlConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace Libiada.Database
+{
+    using Npgsql;
+    using System.Data.Entity;
+
+    public class NpgSqlConfiguration : DbConfiguration
+    {
+        public NpgSqlConfiguration()
+        {
+            const string name = "Npgsql";
+
+            SetProviderFactory(providerInvariantName: name,
+                               providerFactory: NpgsqlFactory.Instance);
+
+            SetProviderServices(providerInvariantName: name,
+                                provider: NpgsqlServices.Instance);
+
+            SetDefaultConnectionFactory(connectionFactory: new NpgsqlConnectionFactory());
+        }
+    }
+}
