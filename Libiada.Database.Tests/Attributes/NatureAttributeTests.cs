88741b345c28ef63af4d152651bf8f8815d6da3f
Nikolay Pozdnichenko <nick680@gmail.com>
2023-12-19 20:00:32 +0600

    Add tests

---
+++ Libiada.Database.Tests/Attributes/NatureAttributeTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Libiada.Database.Tests.Attributes
+{
+    using System;
+
+    using LibiadaCore.Extensions;
+
+    using Libiada.Database.Attributes;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The nature attribute tests.
+    /// </summary>
+    [TestFixture(TestOf = typeof(NatureAttribute))]
+    public class NatureAttributeTests
+    {
+        /// <summary>
+        /// Invalid nature value test.
+        /// </summary>
+        [Test]
+        public void InvalidNatureValueTest()
+        {
+            Assert.Throws<ArgumentException>(() => new NatureAttribute((Nature)10));
+            Assert.Throws<ArgumentException>(() => new NatureAttribute((Nature)0));
+        }
+
+        /// <summary>
+        /// Nature attribute value test.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        [Test]
+        public void NatureAttributeValueTest([Values] Nature value)
+        {
+            var attribute = new NatureAttribute(value);
+            Assert.That(attribute.Value, Is.EqualTo(value));
+            Assert.That(attribute.Value.GetDisplayValue(), Is.EqualTo(value.GetDisplayValue()));
+        }
+    }
+}
