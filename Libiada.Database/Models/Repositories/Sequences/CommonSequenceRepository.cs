21b6c0512ff4a69a8283411958707b333ee82bfa
Nikolay Pozdnichenko <nick680@gmail.com>
2024-03-17 14:30:34 +0600

    Update nuget libraries

--- Libiada.Database/Models/Repositories/Sequences/CommonSequenceRepository.cs
+++ Libiada.Database/Models/Repositories/Sequences/CommonSequenceRepository.cs
@@ -14,6 +14,7 @@ using Libiada.Database.Attributes;
 using Libiada.Database.Extensions;
 
 using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp.PixelFormats;
 
 
 /// <summary>
@@ -87,7 +88,7 @@ public class CommonSequenceRepository : SequenceImporter, ICommonSequenceReposit
             throw new Exception("Cannot find sequence to return");
         }
 
-        var image = Image.Load(imageSequence.Matter.Source);
+        var image = Image.Load<Rgba32>(imageSequence.Matter.Source);
         var orderExtractor = imageSequence.OrderExtractor.GetAttribute<ImageOrderExtractor, ImageOrderExtractorAttribute>().Value;
         var sequence = ImageProcessor.ProcessImage(image, new IImageTransformer[0], new IMatrixTransformer[0], (IImageOrderExtractor)Activator.CreateInstance(orderExtractor));
         var alphabet = new Alphabet { NullValue.Instance() };
