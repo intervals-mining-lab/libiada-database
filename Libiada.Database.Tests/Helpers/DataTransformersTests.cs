b7be60f9606c53cd0a17fe0a954782b3b1efbb36
Matthew <94904232+kquezel@users.noreply.github.com>
2025-05-08 15:40:36 +0600

    add tests for Helpers such as DataTransformers and FileHelper

---
+++ Libiada.Database.Tests/Helpers/DataTransformersTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Libiada.Web.Tests.Helpers;
+
+using Libiada.Database.Helpers;
+using Libiada.Database.Tests;
+
+[TestFixture(TestOf = typeof(DataTransformers))]
+public class DataTransformersTests
+{
+    /// <summary>
+    /// Cleans the fasta file null character test.
+    /// </summary>
+    [Test]
+    public void CleanFastaFileNullCharTest()
+    {
+        string input = "ACGT\0TGCA";
+        string expected = "ACGTTGCA";
+        string result = DataTransformers.CleanFastaFile(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Length, Is.EqualTo(8));
+            Assert.That(result, Is.TypeOf<string>());
+        });
+    }
+
+    /// <summary>
+    /// Cleans the fasta file tab test.
+    /// </summary>
+    [Test]
+    public void CleanFastaFileTabTest()
+    {
+        string input = "ACGT\tTGCA";
+        string expected = "ACGTTGCA";
+        string result = DataTransformers.CleanFastaFile(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Length, Is.EqualTo(8));
+            Assert.That(result, Is.TypeOf<string>());
+        });
+    }
+
+    /// <summary>
+    /// Cleans the fasta file mixed (with both null and tab)  test.
+    /// </summary>
+    [Test]
+    public void CleanFastaFileMixedTest()
+    {
+        string input = "AC\0GT\tTG\0CA";
+        string expected = "ACGTTGCA";
+        string result = DataTransformers.CleanFastaFile(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Length, Is.EqualTo(8));
+            Assert.That(result, Is.TypeOf<string>());
+        });
+    }
+
+    /// <summary>
+    /// Cleans the fasta file clean string test.
+    /// </summary>
+    [Test]
+    public void CleanFastaFileCleanStringTest()
+    {
+        string input = "ACGTTGCA";
+        string expected = "ACGTTGCA";
+        string result = DataTransformers.CleanFastaFile(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Length, Is.EqualTo(8));
+            Assert.That(result, Is.TypeOf<string>());
+            Assert.That(result, Is.EqualTo(input));
+        });
+    }
+}
---
+++ Libiada.Database.Tests/Helpers/FileHelperTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Libiada.Web.Tests.Helpers;
+
+using Libiada.Database.Helpers;
+using Libiada.Database.Tests;
+using System.Text;
+
+[TestFixture(TestOf = typeof(FileHelper))]
+public class FileHelperTests
+{
+    /// <summary>
+    /// Reads the sequence from stream simple text test.
+    /// </summary>
+    [Test]
+    public void ReadSequenceFromStreamSimpleTextTest()
+    {
+        string expected = "Hello World";
+        byte[] bytes = Encoding.UTF8.GetBytes(expected);
+        using MemoryStream stream = new(bytes);
+
+        string result = FileHelper.ReadSequenceFromStream(stream);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Length, Is.EqualTo(11));
+            Assert.That(result, Is.TypeOf<string>());
+        });
+    }
+
+    /// <summary>
+    /// Reads the sequence from stream empty test.
+    /// </summary>
+    [Test]
+    public void ReadSequenceFromStreamEmptyTest()
+    {
+        using MemoryStream stream = new();
+
+        string result = FileHelper.ReadSequenceFromStream(stream);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Empty);
+            Assert.That(result.Length, Is.EqualTo(0));
+            Assert.That(result, Is.TypeOf<string>());
+        });
+    }
+
+    /// <summary>
+    /// Reads the sequence from stream special characters test.
+    /// </summary>
+    [Test]
+    public void ReadSequenceFromStreamSpecialCharactersTest()
+    {
+        string expected = "Hello\nWorld\t!@#$%^&*";
+        byte[] bytes = Encoding.UTF8.GetBytes(expected);
+        using MemoryStream stream = new(bytes);
+
+        string result = FileHelper.ReadSequenceFromStream(stream);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Length, Is.EqualTo(20));
+            Assert.That(result, Is.TypeOf<string>());
+        });
+    }
+}
